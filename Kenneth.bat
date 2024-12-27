f6c3a3bfc05bd6ef8c7df4e3e5225ebf691a32a4
Jasn-Code1 <139421008+Jasn-Code1@users.noreply.github.com>
2024-12-27 11:36:49 +0800

    Update Kenneth.bat

--- Kenneth.bat
+++ Kenneth.bat
@@ -1,1 +1,11 @@
-start https://youtu.be/xvFZjo5PgG0?si=BQgJX_F6ZIdLKOVi
+@if (@CodeSection == @Batch) @then
+@echo off
+    set SendKeys=CScript //nologo //E:JScript "%~F0"
+    cls
+    color 0a
+    start https://youtu.be/xvFZjo5PgG0?si=BQgJX_F6ZIdLKOVi
+    timeout /t 4
+    %SendKeys% {"f"}
+@end
+var WshShell = WScript.CreateObject("WScript.Shell");
+WshShell.SendKeys(WScript.Arguments(0));
